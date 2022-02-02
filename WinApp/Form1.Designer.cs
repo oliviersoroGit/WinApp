61af40ca746c5bd7700e6c81fd1e04fbb3e81d4f
Nacoumblé Olivier Martial SORO <olivier.soro@outlook.com>
2022-02-02 15:42:34 -0500

    AddPRoject

---
+++ WinApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultat = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(74, 32);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 26);
+            this.txt1.TabIndex = 0;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(258, 31);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 26);
+            this.txt2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            // 
+            // lblResultat
+            // 
+            this.lblResultat.AutoSize = true;
+            this.lblResultat.Location = new System.Drawing.Point(514, 35);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(18, 20);
+            this.lblResultat.TabIndex = 4;
+            this.lblResultat.Text = "0";
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(409, 30);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(79, 28);
+            this.btnValider.TabIndex = 5;
+            this.btnValider.Text = "=";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 185);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.lblResultat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "WinApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResultat;
+        private System.Windows.Forms.Button btnValider;
+    }
+}
+
